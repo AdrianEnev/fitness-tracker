e42a40a1f60c0e2fa6c675cb56e692df5d922a31
BuildTools <unconfigured@null.spigotmc.org>
2024-05-04 22:22:36 +0300

    DObavih bibioletka za .env no za sega ne q polzvam. Otdelno za baza danni izpolzvam Edamam a ne USDA. USDA bazata danni e v useAddFood text file koito beshe zamenen s useGetNutrients izpolzvash axios za da poiska informaciq ot edamam

--- app/use/useGetNutrients.tsx
+++ app/use/useGetNutrients.tsx
@@ -1,22 +1,52 @@
+import axios from "axios";
+
+const APP_ID = '90e2036b';
+const APP_KEY = 'b1e82289782395cb48b50b4b11520754';
+
+const fetchFoodData = async (search: any) => {
+  try {
+    const response = await axios.get(
+      `https://api.edamam.com/api/food-database/v2/parser?ingr=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    return response.data.hints; // Return array of food items
+  } catch (error) {
+    console.error('Error fetching food data:', error);
+    throw error; // Rethrow error to handle it outside
+  }
+};
+
 const getNutrients = async (search: any, inputedGrams: any) => {
-   
-    const randomCalories = Math.floor(Math.random() * 500); 
-    const randomProtein = Math.random() * 50; 
-    const randomCarbs = Math.random() * 100; 
-    const randomFat = Math.random() * 50; 
 
-    // Create a mock response object
-    const mockResponse = {
-        title: search,
-        calories: randomCalories,
-        protein: randomProtein,
-        carbs: randomCarbs,
-        fat: randomFat,
-        grams: inputedGrams
-    };
+    if (inputedGrams == 0 || inputedGrams == null) {
+        inputedGrams = 100;
+    }
 
-    // Return the mock response
-    return [mockResponse];
-}
+    try {
+        const foodItems = await fetchFoodData(search);
+        
+        // Map through each food item and extract nutrient information
+        const nutrients = foodItems.map((item: any) => {
+            const food = item.food;
+            const calories = (food.nutrients.ENERC_KCAL / 100) * inputedGrams;
+            const protein = (food.nutrients.PROCNT / 100) * inputedGrams;
+            const carbs = (food.nutrients.CHOCDF / 100) * inputedGrams;
+            const fat = (food.nutrients.FAT / 100) * inputedGrams;
+      
+            return {
+              title: food.label,
+              calories: calories,
+              protein: protein,
+              carbs: carbs,
+              fat: fat,
+              grams: inputedGrams
+            };
+          });
+
+        return nutrients;
+    } catch (error) {
+        console.error('Error getting nutrients:', error);
+        throw error; // Rethrow error to handle it outside
+    }
+};
 
 export default getNutrients;
