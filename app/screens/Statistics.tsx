60b427dfecbb1fc17121c54b4f843d926902794c
BuildTools <unconfigured@null.spigotmc.org>
2024-07-27 11:39:29 +0300

    Dobaveni neshta kum statistics page

--- app/screens/Statistics.tsx
+++ app/screens/Statistics.tsx
@@ -18,8 +18,12 @@ const Statistics = () => {
     const statisticsDocRef = doc(userInfoCollectionRef, "statistics");
 
     const [formattedTotalWorkoutsDuration, setFormattedTotalWorkoutsDuration] = useState("");
+    const [formattedAverageWorkoutDuration, setFormattedAverageWorkoutDuration] = useState("");
+    const [formattedLastWorkoutDate, setFormattedLastWorkoutDate] = useState("");
 
     const [getTotalWorkoutsDurationRan, setGetTotalWorkoutsDurationRan] = useState(false);
+    const [getAverageWorkoutDurationRan, setGetAverageWorkoutDurationRan] = useState(false);
+    const [getLastWorkoutDateRan, setGetLastWorkoutDateRan] = useState(false);
 
     const getTotalWorkoutsDuration = async () => {
 
@@ -82,6 +86,93 @@ const Statistics = () => {
         setGetTotalWorkoutsDurationRan(true);
     };
 
+    const getAverageWorkoutDuration = async () => {
+        const savedWorkoutsCollectionRef = collection(userDocRef, "saved_workouts");
+
+        getDocs(savedWorkoutsCollectionRef).then((querySnapshot) => {
+            let totalDuration = 0; // Initialize a local variable to accumulate durations
+            let workoutCount = 0; // Initialize a local variable to count the number of workouts
+            querySnapshot.forEach((doc) => {
+                totalDuration += doc.data().duration; // Accumulate durations
+                workoutCount++; // Increment workout count
+            });
+
+            const averageDuration = totalDuration / workoutCount; // Calculate average duration
+
+            // Convert averageDuration to hours, minutes, and seconds
+            const hours = Math.floor(averageDuration / 3600);
+            const minutes = Math.floor((averageDuration % 3600) / 60);
+            const seconds = averageDuration % 60;
+
+            setFormattedAverageWorkoutDuration(
+                (() => {
+                    switch (true) {
+                        case hours === 0 && minutes === 0 && seconds === 1:
+                            return `${seconds} second`;
+                        case hours === 0 && minutes === 0 && seconds > 1:
+                            return `${seconds} seconds`;
+                        case hours === 0 && minutes === 1 && seconds === 1:
+                            return `${minutes} minute and ${seconds} second`;
+                        case hours === 0 && minutes > 1 && seconds === 1:
+                            return `${minutes} minutes and ${seconds} second`;
+                        case hours === 0 && minutes === 1 && seconds > 1:
+                            return `${minutes} minute and ${seconds} seconds`;
+                        case hours === 0 && minutes > 1 && seconds > 1:
+                            return `${minutes} minutes and ${seconds} seconds`;
+                        case hours === 1 && minutes === 1:
+                            return `${hours} hour and ${minutes} minute`;
+                        case hours === 1 && minutes > 1:
+                            return `${hours} hour and ${minutes} minutes`;
+                        case hours === 1 && minutes === 0 && seconds === 0:
+                            return `${hours} hour`;
+                        case hours > 1 && minutes === 0 && seconds === 0:
+                            return `${hours} hours`;
+                        case hours > 1 && minutes === 1:
+                            return `${hours} hours and ${minutes} minute`;
+                        case hours > 1 && minutes > 1:
+                            return `${hours} hours and ${minutes} minutes`;
+                        case hours === 1 && minutes === 0 && seconds >= 1:
+                            return `${hours} hour`;
+                        case hours > 1 && minutes === 0 && seconds >= 1:
+                            return `${hours} hours`;
+                        default:
+                            return `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+                    }
+                })()
+            );
+        }).catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+
+        setGetAverageWorkoutDurationRan(true);
+    };
+
+    const getLastWorkoutDate = async () => {
+        const savedWorkoutsCollectionRef = collection(userDocRef, "saved_workouts");
+
+        getDocs(savedWorkoutsCollectionRef).then((querySnapshot) => {
+            let lastWorkoutDate = new Date(0); // Initialize a local variable to store the last workout date
+            querySnapshot.forEach((doc) => {
+                const workoutDate = doc.data().created.toDate(); // Get the workout date
+                if (workoutDate > lastWorkoutDate) {
+                    lastWorkoutDate = workoutDate; // Update the last workout date
+                }
+            });
+
+            const day = lastWorkoutDate.getDate().toString().padStart(2, '0'); // Get the day and pad with zero if necessary
+            const month = (lastWorkoutDate.getMonth() + 1).toString().padStart(2, '0'); // Get the month and pad with zero if necessary
+            const year = lastWorkoutDate.getFullYear();
+
+            const formattedDate = `${day}.${month}.${year}`; // Format the last workout date
+
+            setFormattedLastWorkoutDate(formattedDate);
+        }).catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+
+        setGetLastWorkoutDateRan(true);
+    }
+
 
     useEffect(() => {
         const unsubscribe = onSnapshot(statisticsDocRef, (doc) => {
@@ -91,6 +182,15 @@ const Statistics = () => {
 
         if (!getTotalWorkoutsDurationRan) {
             getTotalWorkoutsDuration();
+        }
+
+        if (!getAverageWorkoutDurationRan) {
+            getAverageWorkoutDuration();
+        }
+
+        if (!getLastWorkoutDateRan) {
+            getLastWorkoutDate();
+
         }
 
         // Cleanup function
@@ -110,8 +210,8 @@ const Statistics = () => {
                 <Text style={tw`m-3 text-lg font-medium`}>Total-weight-lifted: {weightLifted} KG</Text>
                 <Text style={tw`m-3 text-lg font-medium`}>Number-of-workouts: {workoutsFinished}</Text>
                 <Text style={tw`m-3 text-lg font-medium`}>Total workouts duration: {formattedTotalWorkoutsDuration}</Text>
-                <Text style={tw`m-3 text-lg font-medium`}>Average workout duration</Text>
-                <Text style={tw`m-3 text-lg font-medium`}>Last workout</Text>
+                <Text style={tw`m-3 text-lg font-medium`}>Average workout duration: {formattedAverageWorkoutDuration}</Text>
+                <Text style={tw`m-3 text-lg font-medium`}>Last workout: {formattedLastWorkoutDate}</Text>
             </View>
         </View>
     )
