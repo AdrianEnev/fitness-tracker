0e3ae97f7af54991a8f696454aa1dad1225e70d3
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 21:08:03 +0300

    opraven problem v settingsmacros

--- app/screens/SettingsMacros.tsx
+++ app/screens/SettingsMacros.tsx
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableWithoutFeedback, Keyboard, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, FlatList, TouchableWithoutFeedback, Keyboard, TextInput, SafeAreaView } from 'react-native';
 import tw from "twrnc";
 import { collection, doc, getDoc, onSnapshot, setDoc } from 'firebase/firestore';
 import { FIREBASE_AUTH, FIRESTORE_DB } from '../../firebaseConfig';
 import i18next from '../../services/i18next';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomNavigationBar from '../components/BottomNavigationBar';
 import { useTranslation } from 'react-i18next';
 
-const Settings = ({navigation}: any) => {
+interface Nutrient {
+    key: string;
+    value: number;
+}
 
+const Settings = ({navigation}: any) => {
     const usersCollectionRef = collection(FIRESTORE_DB, 'users');
     const userDocRef = doc(usersCollectionRef, FIREBASE_AUTH.currentUser?.uid);
     const userInfoCollectionRef = collection(userDocRef, 'user_info');
     const nutrientsDocRef = doc(userInfoCollectionRef, 'nutrients');
 
-    let [nutrients, setNutrients] = useState({});
-    let [tempNutrients, setTempNutrients] = useState({});
+    const [nutrients, setNutrients] = useState<Record<string, number>>({});
+    const [tempNutrients, setTempNutrients] = useState<Record<string, number>>({});
 
     const updateNutrients = async () => {
-        const docSnap = await getDoc(nutrientsDocRef);
-        if (docSnap.exists()) {
-            setNutrients(docSnap.data());
-            setTempNutrients(docSnap.data()); // Initialize tempNutrients with the current nutrients
-        } else {
-            console.log("No such document!");
+        try {
+            const docSnap = await getDoc(nutrientsDocRef);
+            if (docSnap.exists()) {
+                const data = docSnap.data() as Record<string, number>;
+                setNutrients(data);
+                setTempNutrients(data);
+            } else {
+                console.log("No such document!");
+            }
+        } catch (err) {
+            console.error(err);
         }
     };
 
-    const setNutrient = (value: any, nutrientType: any) => {
+    const setNutrient = (value: string, nutrientType: string) => {
         setTempNutrients(prevState => ({
             ...prevState,
-            [nutrientType]: value
+            [nutrientType]: parseFloat(value) || 0
         }));
     };
 
@@ -46,34 +54,32 @@ const Settings = ({navigation}: any) => {
     };
 
     useEffect(() => {
-        onSnapshot(userInfoCollectionRef, () => {
+        const unsubscribe = onSnapshot(userInfoCollectionRef, () => {
             updateNutrients();
         });
+        return () => unsubscribe(); // Cleanup the listener on component unmount
     }, []);
 
-    const nutrientBox = (value: any, title: string) => (
-        <View style={tw`w-[48%]`}>
-            <Text style={tw`text-xl font-medium text-black ml-[6px] mb-1`}>{i18next.t(title)}</Text>
+    const nutrientBox = (value: string, title: string) => (
+        <View style={tw`w-full p-2`}>
+            <Text style={tw`text-xl font-medium text-black ml-2 mb-1`}>{i18next.t(title)}</Text>
             <TextInput 
-                style={tw`w-full h-12 rounded-2xl bg-[#fd1c47] pb-[6px] pl-3 text-white font-medium text-xl`} 
+                style={tw`w-full h-12 rounded-2xl bg-[#fd1c47] pb-2 pl-3 text-white font-medium text-xl`} 
                 keyboardType='number-pad' 
-                defaultValue={value}
+                value={value}
                 maxLength={4} 
                 onChangeText={(text) => setNutrient(text, title)} 
             />
         </View>
     );
 
-    const renderNutrients = ({ item }: any) => (
-        <View style={tw`flex flex-row flex-wrap w-full gap-x-3 gap-y-3 mt-3`}>
-            {nutrientBox(item.calories, 'calories')}
-            {nutrientBox(item.protein, 'protein')}
-            {nutrientBox(item.carbs, 'carbs')}
-            {nutrientBox(item.fat, 'fat')}
-        </View>
+    const nutrientsArray: Nutrient[] = Object.entries(tempNutrients).map(([key, value]) => ({ key, value }));
+
+    const renderItem = ({ item }: { item: Nutrient }) => (
+        nutrientBox(item.value.toString(), item.key)
     );
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     return (
         <SafeAreaView style={tw`w-full h-full bg-white`}>
@@ -82,9 +88,14 @@ const Settings = ({navigation}: any) => {
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={tw`flex-1`}>
                     <View style={tw`w-full bg-white`}>
-                        <View style={tw`mx-3`}>
-                            <FlatList data={[tempNutrients]} renderItem={renderNutrients} />
-                        </View>
+                        <FlatList
+                            data={nutrientsArray}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.key}
+                            numColumns={2} // This will layout items in a grid with 2 columns
+                            columnWrapperStyle={tw`justify-between w-1/2`} // Adjusts spacing between columns
+                            contentContainerStyle={tw`p-3`} // Adds padding around the list
+                        />
                     </View>
                 </View>
             </TouchableWithoutFeedback>
